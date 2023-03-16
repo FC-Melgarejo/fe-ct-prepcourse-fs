21f0f006a4351baff056f3103da575fd38aacf3c
fati <faticarolinamelgarejo2@gmail.com>
2023-03-16 18:44:21 -0300

    mi primer comit

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -1,38 +1,44 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
-function devolverString(string) {
+function devolverString(Hola) {
    // Debe retornar un string.
    // Tu código:
+   return Hola;
 }
+
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   
+   return suma = x + y;
+
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
-   // Tu código:
+  // Tu código:
+  
+  return resta = x - y;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   
+   return divide = x / y;
 }
+
 
 function multiplica(x, y) {
+   return x * y ;
+   
+}
+
    // Retorna el resultado de su multiplicación.
-   // Tu código:
-}
-
-function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
-}
-
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    devolverString,
